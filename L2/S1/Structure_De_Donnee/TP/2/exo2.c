66ac621957549e3b226c9a0c7034f89b57923e59
Macxence Schneider <schneidermacxencepro@gmail.com>
2024-10-02 17:13:27 +0200

    cours

--- L2/S1/Structure_De_Donnee/TP/2/exo2.c
+++ L2/S1/Structure_De_Donnee/TP/2/exo2.c
@@ -2,9 +2,44 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct chainon {
+    int data;
+    struct chainon* next;
+} chainon_t;
 
-int main{
+typedef chainon_t* liste_t;
 
-
-    
+chainon_t * ajouter_chainon(int data, liste_t *l){
+    chainon_t *a = malloc(sizeof(chainon_t));
+    a->data = data;
+    a->next = *l;
+    *l = a;
 }
+
+chainon_t * affichierchainon(liste_t *l){
+    chainon_t *a = *l;
+    if (a->next == NULL){
+        printf("%d ",a->data);
+        return;
+    }else{
+        printf("%d ",a->data); 
+        affichierchainon(a->next);
+    }
+}
+
+int main(){
+    
+    int data = 19;
+    liste_t l = NULL;
+
+    ajouter_chainon(data,&l);
+    affichierchainon(&l);
+
+
+}
+
+
+//Création d'un nouveau chainon 
+//Initialisation de la valeur du chaînon à partir de la donnée fournie 
+//initialisation du pointeur next vers le début de la liste de travail 
+//mise a jour de la liste pour la faire pointer vers le nouveau chainon
